f9c8658c56db2386357154cf9246a1df9b0d8cff
mnour <mnour@mnour-Satellite-C855D>
2016-11-17 12:54:22 +0200

    first commit

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'jschat';
+
+$conn =mysqli_connect($host,$username,$password,$database);
+if(mysqli_connect_errno()){
+	echo 'faild to connect:'.mysqli_connect_error() ;
+}
---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$('#submit').on('click',function(){
+		var name = $('#name').val();
+		var shout = $('#shout').val();
+		var date = getDate();
+		var dataString = 'name=' + name + '&shout=' + shout + '&date' + date;
+
+		// validation
+		if(name == '' || shout == ''){
+			alert('please fill Your name and shout');
+		}else{
+			$.ajax({
+				type:"POST",
+				url:"../jschat/shoutbox.php",
+				data: dataString,
+				cache: false,
+				success: function(html){
+				$('#shout ul').prepend(html);
+				}
+			});
+		}
+
+		return false ;
+	});
+});
+function getDate(){
+	var date;
+	date=new date;
+	date = date.getUTCFullYear() +'-'+
+		('00' + (date.getUTCMonth() +1 )).slice(-2)+'-'+
+		('00' + date.getUTCDate().slice(-2)+' '+
+		('00' + date.getUTCHours().slice(-2)+ ':'+
+		('00' + date.getUTCMinutes().slice(-2)+':'+
+		('00' + date.getUTCSeconds().slice(-2);
+	return date;
+}
