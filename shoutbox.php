96d12af50f9f69d2f2b9def188d83336291ffb51
hunter eyes eg <wadedy2020@gmail.com>
2016-11-18 15:58:14 +0200

    Update shoutbox.php

--- shoutbox.php
+++ shoutbox.php
@@ -1,3 +1,23 @@
-<?php
+<?php 
+   // Database connection
+  include_once 'database.php';  
 
-echo 'hello';
+if(isset($_POST['name']) && isset($_POST['shout'])){
+	$name = mysqli_real_escape_string($con,$_POST['name']);
+	$shout = mysqli_real_escape_string($con,$_POST['shout']);
+	//$date = mysqli_real_escape_string($con,$_POST['date']);
+
+	//set timezone 
+	date_default_timezone_get('America/New_York');
+	$date = date('h:i:s a',time());
+
+
+	$query = "INSERT INTO `shouts` (name,date,shout)
+	VALUES ('$name','$date','$shout')";
+	
+	if(!mysqli_query($con,$query)){
+	echo 'Error:'.mysqli_error($con);
+	}else{
+	echo '<li>'.$name.': '.$shout.':<h5> '.$date.'</h5></li>';
+	}
+}
